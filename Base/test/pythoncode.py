b1665a02548731c32c1f84614abe03d798e24325
nbgao <nbgao@126.com>
2017-08-27 23:52:47 +0800

    magic & Scipy1

---
+++ Base/test/pythoncode.py
@@ -0,0 +1,11 @@
+import numpy as np
+def append_if_not_exists(arr, x):
+    if x not in arr:
+        arr.append(x)
+        
+        
+def some_useless_slow_function():
+    arr = list()
+    for i in range(1000):
+        x = np.random.randint(0, 10000)
+        append_if_not_exists(arr, x)
